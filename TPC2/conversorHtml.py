2147e4fdc6759891ad98390f10a4c3ce60f20b32
joaoprp111 <joaoprp111@gmail.com>
2022-02-26 00:15:19 +0000

    Atores

--- TPC2/conversorHtml.py
+++ TPC2/conversorHtml.py
@@ -1,6 +1,54 @@
 import json
 import os
 
+def generateAtoresPage(actorsId):
+    html = '''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Atores</title>
+            <link rel="stylesheet" href="../css/w3.css">
+            <link rel="stylesheet" href="../css/styling.css">
+        </head>
+        <body>
+            <h1>Lista de atores por ordem alfabética</h1>
+            <ul>
+    '''
+    #Sort actors alphabetically
+    res = list(sorted(actorsId.items(), key=lambda i: i[0]))
+    for a in res:
+        link = 'http://localhost:7777/atores/a' + str(a[1])
+        html += '<li><a href=\"' + link + '\">' + str(a[0]) + '</a></li>'
+    html += '</ul></body></html>'
+    return html
+
+def generateActorPage(actorName, movies, moviesDict):
+    html = '''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>''' + actorName + '''</title>
+            <link rel="stylesheet" href="/css/w3.css">
+            <link rel="stylesheet" href="/css/styling.css">
+        </head>
+        <body>
+            <h1>''' + actorName + '''</h1>
+            <h2>Filmes onde participa</h2>
+            <ul>
+    '''
+    moviesDictKeys = list(moviesDict.keys())
+    moviesDictValues = list(moviesDict.values())
+    for movie in movies:
+        keyPosition = moviesDictValues.index(movie)
+        movieId = moviesDictKeys[keyPosition]
+        html += '<li><a href=\"http://localhost:7777/filmes/f' + str(movieId) + '\">' + movie + '</a></li>'
+        
+    html += '</ul></body></html>'
+    
+    return html
+
 def generateFilmesPage(movies):
     html = '''
     <!DOCTYPE html>
@@ -23,7 +71,7 @@ def generateFilmesPage(movies):
     html += '</ul></body></html>'
     return html
 
-def content(movieContent):
+def content(movieContent, actorsIds):
     contentHtml = '<h1>' + movieContent['title'] + '</h1>' + '<p>' + str(movieContent['year']) + ' | '
     
     for genero in movieContent['genres']:
@@ -33,23 +81,23 @@ def content(movieContent):
     
     elenco = movieContent['cast']
     for ator in elenco:
-        contentHtml += '<li>' + ator + '</li>'
+        contentHtml += '<li><a href=\"http://localhost:7777/atores/a' + str(actorsIds[ator]) + '\">' + ator + '</a></li>'
         
     contentHtml += '</ul>'
     
     return contentHtml
 
-def generateHtml(movieContent, file):
+def generateHtml(movieContent, file, actorsIds):
     html = '''
     <!DOCTYPE html>
     <html>
         <head>
             <meta charset="UTF-8">
             <title>''' + movieContent['title'] + '''</title>
-            <link rel="stylesheet" href="../css/w3.css">
-            <link rel="stylesheet" href="../css/styling.css">
+            <link rel="stylesheet" href="/css/w3.css">
+            <link rel="stylesheet" href="/css/styling.css">
         </head>
-        <body>''' + content(movieContent) + '''</body></html>'''
+        <body>''' + content(movieContent,actorsIds) + '''</body></html>'''
         
     file.write(html)
 
@@ -58,6 +106,11 @@ f = open('dataset.json', encoding='utf-8')
 
 #Dict with id as a key and movie name as a value
 movies = dict()
+
+#Actor's name and its id
+actorsId = dict()
+#Actor's name and its movies
+actorsMovies = dict()
 
 #Html folder
 folderHtml = 'html/'
@@ -69,21 +122,53 @@ if not os.path.exists(folderHtml):
 jsonObj = json.load(f)
 #Movies counter
 count = 1
+actorCount = 1
 
 print('Generating pages...')
 for reg in jsonObj:
-    #Create html file that corresponds to the register
-    filename = folderHtml + 'f' + str(count) + '.html'
-    htmlFile = open(filename,'w',encoding='utf-8')
-    movies[count] = reg['title']
+    movies[count] = reg
     count += 1
-    generateHtml(reg, htmlFile)
+    
+    #Fill actors dictionaires
+    for actor in reg['cast']:
+        #For every actor
+        if actor not in actorsId.keys():
+            actorsId[actor] = actorCount
+            actorCount += 1
+        actorsMovies.setdefault(actor,[])
+        actorsMovies[actor].append(reg['title'])
+    
+#Generate all movies pages
+for (k,v) in movies.items():  
+    #Create html file that corresponds to the movie
+    filename = folderHtml + 'f' + str(k) + '.html'
+    htmlFile = open(filename,'w',encoding='utf-8')
+    generateHtml(v,htmlFile,actorsId)
     htmlFile.close()
     
+#Update movies dict
+for key in movies.keys():
+    temp = movies.get(key)
+    movies[key] = temp['title']
+
 #Generate main page (/filmes)
 htmlMainPage = generateFilmesPage(movies)
 indexFile = open('html/index.html','w',encoding='utf-8')
 indexFile.write(htmlMainPage)
 indexFile.close()
+
+#Generate aditional page (/atores)
+htmlAtores = generateAtoresPage(actorsId)
+atoresFile = open('html/atores.html','w',encoding='utf-8')
+atoresFile.write(htmlAtores)
+atoresFile.close()
+
+#Generate actors pages
+for actor in actorsId.keys():
+    actorFilename = 'html/a' + str(actorsId[actor]) + '.html'
+    htmlActorPage = generateActorPage(actor, actorsMovies[actor], movies)
+    actorFile = open(actorFilename, 'w', encoding='utf-8')
+    actorFile.write(htmlActorPage)
+    actorFile.close()
     
 print('Done!')
--- TPC2/fileserver.js
+++ TPC2/fileserver.js
@@ -5,9 +5,10 @@ var fs = require('fs')
 myserver = http.createServer(function(req, res) {
     console.log('Chegou um pedido: ' + req.url)
     var myurl = url.parse(req.url, true).pathname
-    var movieId = myurl.split("/")[2]
-    if (movieId){
-        fs.readFile('./html/' + movieId + '.html', function(err, data){
+    var content = myurl.split("/")[1]
+    var id = myurl.split("/")[2]
+    if (id && (content == "filmes" || content == "atores")){
+        fs.readFile('./html/' + id + '.html', function(err, data){
             if(err){
                 res.writeHead(404)
             }else{
@@ -19,6 +20,17 @@ myserver = http.createServer(function(req, res) {
     }
     else if (myurl == "/filmes"){
         fs.readFile('./html/index.html', function(err, data){
+            if(err){
+                res.writeHead(404)
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.write(data)
+            }
+            res.end()
+        })
+    }
+    else if (myurl == "/atores"){
+        fs.readFile('./html/atores.html', function(err, data){
             if(err){
                 res.writeHead(404)
             }else{
